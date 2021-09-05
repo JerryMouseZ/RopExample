6e9f0eef1e358b49af1e13557703624976cc6efa
jz <zhangjerryn@gmail.com>
2021-09-05 13:57:27 +0000

    need to analyse the log

--- TraceRop/fork_exec.c
+++ TraceRop/fork_exec.c
@@ -20,6 +20,7 @@ pid_t start_child(const char* program, char **argv)
         ptrace(PTRACE_TRACEME, 0, 0, 0);
         // This code runs in the child process.
         int output_fd = pipefd[1];
+        fcntl(output_fd, F_SETFL, O_NONBLOCK);
         // Replace the child's stdout and stderr handles with the log file handle:
         if (dup2(output_fd, STDOUT_FILENO) < 0) {
             perror("dup2 (stdout)");
@@ -37,6 +38,19 @@ pid_t start_child(const char* program, char **argv)
     return pid;
 }
 
+int print_pipe(int fd)
+{
+    char ch;
+    int ret;
+    // blocking here
+    while((ret = read(fd, &ch, 1)) > 0)
+    {
+        write(1, &ch, 1);
+    }
+    return ret;
+}
+
+
 int main(){
     if(pipe(pipefd) == -1){
         perror("pipe");
@@ -52,25 +66,18 @@ int main(){
     }
 
     close(pipefd[1]);
+    fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
     waitpid(child, &status, 0);
+
     ptrace(PTRACE_CONT, child, 0, 0);
     waitpid(child, &status, 0);
-    ptrace(PTRACE_SINGLESTEP, child, 0, 0);
+    print_pipe(pipefd[0]);
 
-    while(1)
-    {
-        waitpid(child, &status, 0);
-        if(WIFEXITED(status))
-            break;
-        char ch;
-        // blocking here
-        while(read(pipefd[0], &ch, 1) > 0)
-        {
-            printf("%c", ch);
-        }
-        printf("\n");
-        ptrace(PTRACE_DETACH, child, 0, 0);
+    if(WIFEXITED(status)){
+        printf("already done\n");
     }
+
+    ptrace(PTRACE_DETACH, child, 0, 0);
     close(pipefd[0]);
 
     exit(0);
